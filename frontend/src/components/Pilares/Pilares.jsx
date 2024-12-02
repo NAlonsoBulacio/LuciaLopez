86477cd66d3c5f65f29d30add9fdd51a97a786eb
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-12-02 20:07:54 -0300

    icons

--- frontend/src/components/Pilares/Pilares.jsx
+++ frontend/src/components/Pilares/Pilares.jsx
@@ -4,7 +4,7 @@ import {
   impuestos,
   propiedades,
   seguridad,
-  inversion
+  inversion,
 } from "../../assets/index";
 import { motion } from "framer-motion";
 const Pilares = () => {
@@ -18,64 +18,62 @@ const Pilares = () => {
         viewport={{ once: true, amount: 0.3 }}
       >
         {/* <h2 className="text-3xl font-clash-700 text-gray-800 mb-8">Los 6 Pilares de la Salud</h2> */}
-        <div className="flex flex-wrap gap-8 justify-center">
+        <div className="flex flex-wrap gap-4 lg:gap-8 justify-center">
           {/* Aquí agregar las imágenes de los pilares */}
-          <div className="w-1/3 lg:w-1/6 flex flex-col items-center">
+          <div className="w-[43%] lg:w-1/6 flex flex-col items-center">
             <img
               src={inversion}
               alt="Pilar 1"
               className="w-20 h-20 mb-4 rounded-full"
             />
-            <p className="font-clash-400 text-lg text-gray-600">
+            <p className="font-clash-400 text-md lg:text-lg text-gray-600">
               Cero impuestos a la propiedad.
             </p>
           </div>
-                   {/* Repetir para los demás pilares */}
-                   <div className="w-1/3 lg:w-1/6 flex flex-col items-center">
+          {/* Repetir para los demás pilares */}
+          <div className="w-[43%] lg:w-1/6 flex flex-col items-center">
             <img
               src={extranjero}
               alt="Pilar 1"
               className="w-20 h-20 mb-4 rounded-full"
             />
-            <p className="font-clash-400 text-lg text-gray-600">
+            <p className="font-clash-400 text-md lg:text-lg text-gray-600">
               Facilidad de compra para extranjeros.
             </p>
           </div>
           {/* Repetir para los demás pilares */}
-          <div className="w-1/3 lg:w-1/6 flex flex-col items-center">
+          <div className="w-[43%] lg:w-1/6 flex flex-col items-center">
             <img
               src={propiedades}
               alt="Pilar 1"
               className="w-20 h-20 mb-4 rounded-full"
             />
-            <p className="font-clash-400 text-lg text-gray-600">
+            <p className="font-clash-400 text-md lg:text-lg text-gray-600">
               Alta valorización de las propiedades.
             </p>
           </div>
           {/* Repetir para los demás pilares */}
-          <div className="w-1/3 lg:w-1/6 flex flex-col items-center">
+          <div className="w-[43%] lg:w-1/6 flex flex-col items-center">
             <img
               src={seguridad}
               alt="Pilar 1"
               className="w-20 h-20 mb-4 rounded-full"
             />
-            <p className="font-clash-400 text-lg text-gray-600">
+            <p className="font-clash-400 text-md lg:text-lg text-gray-600">
               Seguridad económica y estabilidad jurídica.
             </p>
           </div>
-           {/* Repetir para los demás pilares */}
-           <div className="w-1/3 lg:w-1/6 flex flex-col items-center">
+          {/* Repetir para los demás pilares */}
+          <div className="w-[43%] lg:w-1/6 flex flex-col items-center">
             <img
               src={impuestos}
               alt="Pilar 1"
               className="w-20 h-20 mb-4 rounded-full"
             />
-            <p className="font-clash-400 text-lg text-gray-600">
+            <p className="font-clash-400 text-md lg:text-lg text-gray-600">
               Retornos de inversión de hasta el 12%.
             </p>
           </div>
-
-          
         </div>
       </motion.div>
     </div>
